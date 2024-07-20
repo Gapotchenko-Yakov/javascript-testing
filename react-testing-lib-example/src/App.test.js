966388265656c5e1c12f802d72d149d92f090c9a
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-20 11:55:52 +0300

    Write first tests for react component with RTL. Using screen.debug() and toMatchSnapshot().

--- react-testing-lib-example/src/App.test.js
+++ react-testing-lib-example/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldElement = screen.getByText(/hello world/i);
+  const btnElement = screen.getByRole("button");
+  const inputElement = screen.getByPlaceholderText("input value...");
+  expect(helloWorldElement).toBeInTheDocument();
+  expect(btnElement).toBeInTheDocument();
+  expect(inputElement).toMatchSnapshot();
+  screen.debug();
 });
