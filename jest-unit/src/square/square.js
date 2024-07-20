2a8175045c55a5b7ff9ae27b9043bba776e995b3
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-20 10:26:23 +0300

    add beforeEach and beforeAll functions.

--- jest-unit/src/square/square.js
+++ jest-unit/src/square/square.js
@@ -1,3 +1,9 @@
-const square = (number) => number * number;
+const square = (number) => {
+  if (number === 1) {
+    return 1;
+  }
+
+  return Math.pow(number, 2);
+};
 
 module.exports = square;
--- jest-unit/src/square/square.test.js
+++ jest-unit/src/square/square.test.js
@@ -1,10 +1,23 @@
 const square = require("./square");
 
 describe("square", () => {
+  let mockValue;
+  // Перед каждым тестом
+  beforeEach(() => {});
+  // Один раз перед всеми тестами
+  beforeAll(() => {});
+
   test("Корректное значение", () => {
-    expect(square(2)).toBeGreaterThan(3);
-    expect(square(2)).toBe(4);
-    expect(square(2)).toBeLessThan(5);
-    expect(square(2)).not.toBeUndefined();
+    // expect(square(2)).toBeGreaterThan(3);
+    // expect(square(2)).toBe(4);
+    // expect(square(2)).toBeLessThan(5);
+    // expect(square(2)).not.toBeUndefined();
   });
+
+  // Перед каждым тестом
+  afterEach(() => {
+    // удалить из БД
+  });
+  // Один раз перед всеми тестами
+  afterAll(() => {});
 });
