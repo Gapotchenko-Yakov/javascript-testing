e9b44e72c269cfc743bb754aae7c77955f341e5b
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-20 11:45:47 +0300

    initial commit for react-tsting-lib-example project

---
+++ react-testing-lib-example/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <button>Click me</button>
+      <input type="text" placeholder="input value..." />
+    </div>
+  );
+}
+
+export default App;
