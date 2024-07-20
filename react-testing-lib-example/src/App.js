bd286088d9f6bbdd87503590af1c5d36848a2cb4
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-07-20 12:41:20 +0300

    Write test using screen.findByText query

--- react-testing-lib-example/src/App.js
+++ react-testing-lib-example/src/App.js
@@ -1,9 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    setTimeout(() => setData({}), 100);
+  }, []);
+
   return (
     <div>
+      {data && <div style={{ color: "red" }}>data</div>}
       <h1>Hello World</h1>
       <button>Click me</button>
       <input type="text" placeholder="input value..." />
--- react-testing-lib-example/src/App.test.js
+++ react-testing-lib-example/src/App.test.js
@@ -1,13 +1,26 @@
-import { render, screen } from "@testing-library/react";
+import { findByText, render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const helloWorldElement = screen.getByText(/hello world/i);
-  const btnElement = screen.getByRole("button");
-  const inputElement = screen.getByPlaceholderText("input value...");
-  expect(helloWorldElement).toBeInTheDocument();
-  expect(btnElement).toBeInTheDocument();
-  expect(inputElement).toMatchSnapshot();
-  screen.debug();
+describe("TEST APP", () => {
+  test("renders learn react hello", () => {
+    render(<App />);
+    const helloWorldElement = screen.getByText(/hello world/i);
+    const btnElement = screen.getByRole("button");
+    const inputElement = screen.getByPlaceholderText("input value...");
+    expect(helloWorldElement).toBeInTheDocument();
+    expect(btnElement).toBeInTheDocument();
+    expect(inputElement).toMatchSnapshot();
+    // screen.debug();
+  });
+
+  test("renders learn react hello", async () => {
+    render(<App />);
+    // const helloWorldElement = screen.queryByText(/hello world 2/i);
+    // expect(helloWorldElement).toBeNull();
+    screen.debug();
+    const helloWorldElement = await screen.findByText(/data/i);
+    expect(helloWorldElement).toBeInTheDocument();
+    expect(helloWorldElement).toHaveStyle({ color: "red" });
+    screen.debug();
+  });
 });
